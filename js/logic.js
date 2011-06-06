1dea2e757e84e1ee27b95de198b4b4d2f8e60221
chris <porios@yahoo.gr>
2011-06-07 01:52:19 +0300

    Score calculator added

--- js/logic.js
+++ js/logic.js
@@ -61,6 +61,7 @@ function read()
     canvas.width = W * 20;
     canvas.height = H * 20;
     draw();
+    updatescore();
 }
 
 function write()
@@ -101,7 +102,7 @@ function click(mx, my)
     y = Math.floor(my/20);
     field[x][y] = next[field[x][y]];
     ctx.drawImage(img[field[x][y]], x * 20, y *20);
-
+    updatescore();
 }
 
 function draw()
@@ -112,4 +113,66 @@ function draw()
             ctx.drawImage(img[field[i][j]], i * 20, j *20);
         }
     }
+}
+
+function updatescore()
+{
+    var vis = new Array;
+    var i, s, sside, x, y, d, len, L, S;
+    vis[0] = new Array; vis[1] = new Array; vis[2] = new Array; vis[3] = new Array;
+    
+    S=0;
+
+    for(s=0;s<4;s++)
+    {
+
+        if(s==0){ sside = 'L'; L = H; }
+        else if(s==1){ sside = 'R'; L = H;}
+        else if(s==2){ sside = 'U'; L = W; }
+        else if(s==3){ sside = 'D'; L = W; }
+        
+        for(i=0;i<L;i++)
+        {
+            if(vis[s][i]==true) continue;
+
+            if(sside=='L'){ x = 0; y = i; d = 'R'}
+            else if(sside=='R'){ x = W-1; y = i; d = 'L'}
+            else if(sside=='U'){ x = i; y=0; d='D' }
+            else if(sside=='D'){ x = i; y=H-1; d='U' }
+            len = 0;
+
+            vis[s][i] = true;
+            
+            while(1)
+            {
+                if(x>=W){ vis[1][y]=true; break; }
+                if(x<0){ vis[0][y]=true; break; }
+                if(y>=H) { vis[3][x]=true; break; }
+                if(y<0) { vis[2][x]=true; break; }
+                if(field[x][y]=='x') break;
+
+                if(field[x][y]=='/')
+                {
+                    if(d=='R') d='U';
+                    else if(d=='D') d='L';
+                    else if(d=='L') d='D';
+                    else if(d=='U') d='R';
+                }
+                else if(field[x][y]=='\\')
+                {
+                    if(d=='R') d='D';
+                    else if(d=='D') d='R';
+                    else if(d=='L') d='U';
+                    else if(d=='U') d='L';
+                }
+
+                if(d=='R') x++; if(d=='L') x--; if(d=='U') y--; if(d=='D') y++;
+                len++;
+            }
+            
+            S += Math.pow(len,2);
+        }
+    }
+
+    $("div#score").html(S)
 }
